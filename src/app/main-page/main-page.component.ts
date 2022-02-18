1cdfc21b1c0c558c5acc159d22db8d87d9ca798b
Thomas Bent <thomas.bentlohner@outlook.de>
2022-02-18 22:13:48 +0100

    landing page design

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  heading: string = 'ATypical';
+
   constructor() { }
 
   ngOnInit(): void {
